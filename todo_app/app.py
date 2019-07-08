0d3732cb09207c9fbc48a569b5e4dd39bdfe281b
Himank Bhalla <himank.bhalla@codingblocks.com>
2019-07-08 08:42:29 +0530

    post handler and form

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,35 @@
-from flask import Flask , render_template
-
+from flask import Flask , render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_database.db'
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200) , nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __str__(self):
+        return 'Task: {}'.format(self.id)
+
+@app.route('/', methods=['GET' , 'POST'])
 def index():
-    return render_template('index.html')
+    print('here ', request.method)
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error adding your task'
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
